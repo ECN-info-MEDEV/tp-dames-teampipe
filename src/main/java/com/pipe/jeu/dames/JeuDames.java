5c4236b49da5bc48630a847cae98168492463dca
Diego Felipe Lopez Avila <dlopezav@unal.edu.co>
2023-12-07 09:49:16 +0100

    init

---
+++ src/main/java/com/pipe/jeu/dames/JeuDames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pipe.jeu.dames;
+
+/**
+ *
+ * @author felipelavila
+ */
+public class JeuDames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
